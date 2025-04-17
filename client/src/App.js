0993e3978e9e99630e1d1ff89eccc1c3d8448e81
LaibaYaseen002 <laibayaseen002@gmail.com>
2025-04-17 15:57:19 +0500

    Added Node modules in gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { useAuth } from "./context/AuthContext";
+import { useAuth } from "./contexts/AuthContext";
 import TodoList from "./components/TodoList";
 import AuthForm from "./components/AuthForm";
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./style.css"; // your global styles (optional)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
